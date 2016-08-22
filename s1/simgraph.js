854226cca56f5d842733a09d91f7f69fa80be089
root <gasperoni.simone@gmail.com>
2016-08-22 21:57:29 +0200

    s1: dfs structure update

--- s1/simgraph.js
+++ s1/simgraph.js
@@ -1,10 +1,17 @@
-//a minimal representation for graph by simone gasperoni
+//graph minimal representation
+
+function contains(a, obj) {
+   	var i = a.length;
+  	while (i--) if (a[i]===obj) return true;
+   	return false;
+}
 
 function Simvertex(vertex){
 	this.vertex=vertex;
 	this.linkto=[];
 	this.getVertex=getVertex;
 	this.getLinks=getLinks;
+	this.getLinks2=getLinks2;
 	this.addLinkto=addLinkto;
 
 	function getVertex(){
@@ -13,20 +20,50 @@ function Simvertex(vertex){
 	function getLinks(){
 		return this.linkto;
 	}
-	function contains(a, obj) {
-    	var i = a.length;
-    	while (i--) {
-       		if (a[i] === obj) {
-           		return true;
-       		}
-    	}
-    	return false;
+	function getLinks2(){
+		var array=[]
+		for (var i=0; i<this.linkto.length; i++) {
+			array[i]=this.linkto[i].getVertex();
+		}
+		return array;
 	}
-	function addLinkto(vertexlinked){
-		if (!contains(this.linkto, vertexlinked)) this.linkto[this.linkto.length]=vertexlinked;
+	function addLinkto(verticesLinked){
+		for (var i=0; i<verticesLinked.length; i++) {
+			if (!contains(this.linkto, verticesLinked[i])){
+				this.linkto[this.linkto.length]=verticesLinked[i];
+			}
+		}
 	}
 }
 
 function Simgraph(){
-	//...
+	this.vertices=[];
+	this.initialstates=[];
+	this.getInitialStates=getInitialStates;
+	this.setInitialStates=setInitialStates;
+	this.getVertices=getVertices;
+	this.addVertex=addVertex;
+	this.print=print;
+	function setInitialStates(states){
+		this.initialstates=states;
+	}
+	function getInitialStates(){
+		return this.initialstates;
+	}
+	
+	function addVertex(vertex){
+		if (!contains(this.vertices, vertex))
+			this.vertices[this.vertices.length]=vertex;
+	}
+	function getVertices(){
+		return this.vertices;
+	}
+	function print(){
+		for (var i=0; i<this.vertices.length; i++) {
+			document.write("---------------------------- <br>");
+			document.write("node: "+this.vertices[i].getVertex()+" <br>");
+			document.write("links to "+this.vertices[i].getLinks2()+" <br>");
+			document.write("---------------------------- <br>");
+		}
+	}
 }
